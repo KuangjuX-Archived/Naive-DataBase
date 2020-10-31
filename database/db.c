86bdb2f78777e40f98e1b8385677e4a25b129636
KuangjuX <18630816527@163.com>
2020-10-31 20:44:27 +0800

    part2

--- database/db.c
+++ database/db.c
@@ -58,11 +58,33 @@ int main(int argc, char* argv[]){
         read_input(input_buffer);
 
         if(strcmp(input_buffer->buffer ,".exit") == 0){
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        }else{
-            printf("Unrecognized command '%s' .\n",input_buffer->buffer);
+        //     close_input_buffer(input_buffer);
+        //     exit(EXIT_SUCCESS);
+        // }else{
+        //     printf("Unrecognized command '%s' .\n",input_buffer->buffer);
+        // }
+
+        if(input_buffer->buffer[0] == '.'){
+            switch (do_meta_command(input_buffer)){
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECONGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                    continue;    
+            }
         }
+
+        Statement statement;
+        switch(prepare_statement(input_buffer,&statement)){
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'.\n",input_buffer->buffer);
+                continue;
+        }
+
+        execute_statement(&statement);
+        printf("Executed.\n");
         
     }
 }
