a6dccf8b10e8c329fa2e289df3d948900f3842cf
KuangjuX <18630816527@163.com>
2020-10-31 20:44:35 +0800

    part2

--- database/db.c
+++ database/db.c
@@ -12,6 +12,19 @@ typedef struct {
     ssize_t input_length;
 }InputBuffer;
 
+typedef enum{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+}MetaCommandResult;
+
+typedef enum{PREPARE_SUCCESS,PREPARE_UNRECOGNIZED_STATEMENT} PrepareResult;
+
+typedef enum {STATEMENT_INSERT,STATEMENT_SELECT} StatementType;
+
+typedef struct {
+    StatementType type;
+}Statement;
+
 ssize_t getline(char **lineptr, size_t *n, FILE* stream);
 /**
  * lineptr : a pointer to the variable we use to point to the buffer containing the read line. If it set to NULL it is mallocatted by getline and should thus be freed by the user, even if the command fails.
@@ -21,14 +34,14 @@ ssize_t getline(char **lineptr, size_t *n, FILE* stream);
  * We tell getline to store the read line in input_buffer->buffer and the size of the allocated buffer in input_buffer->buffer_length. We store the return value in input_buffer->input_length.
  * buffer starts as null, so getline allocates enough memory to hold the line of input and makes buffer point to it.
 **/
-InputBuffer* new_input_buffer() {
-    InputBuffer* input_buffer = (InputBuffer*)(malloc)(sizeof(InputBuffer));
-    input_buffer->buffer = NULL;
-    input_buffer->buffer_length = 0;
-    input_buffer->input_length = 0;
+// InputBuffer* new_input_buffer() {
+//     InputBuffer* input_buffer = (InputBuffer*)(malloc)(sizeof(InputBuffer));
+//     input_buffer->buffer = NULL;
+//     input_buffer->buffer_length = 0;
+//     input_buffer->input_length = 0;
 
-    return input_buffer;
-}
+//     return input_buffer;
+// }
 
 void read_input(InputBuffer* input_buffer){
     ssize_t bytes_read = getline(&(input_buffer),&(input_buffer->buffer_length),stdin);
@@ -50,6 +63,44 @@ void close_input_buffer(InputBuffer* input_buffer){
 void print_prompt(){
     printf("db > ");
 }
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer){
+    if(strcmp(input_buffer->buffer,".exit")==0){
+        exit(EXIT_SUCCESS);
+    }else
+    {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+    
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
+    if(strncmp(input_buffer->buffer,"insert",6) == 0){
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+
+    if(strcmp(input_buffer->buffer,"select") == 0){
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement){
+    switch (statement->type){
+        case(STATEMENT_INSERT):
+            printf("This is where we would do an insert.\n");
+            break;
+        
+        case(STATEMENT_SELECT):
+            printf("This is where we would do a select.\n");
+            break;
+        
+    }
+}
+
 int main(int argc, char* argv[]){
     InputBuffer* input_buffer = new_input_buffer();
     //loop
@@ -57,7 +108,7 @@ int main(int argc, char* argv[]){
         print_prompt();
         read_input(input_buffer);
 
-        if(strcmp(input_buffer->buffer ,".exit") == 0){
+        // if(strcmp(input_buffer->buffer ,".exit") == 0){
         //     close_input_buffer(input_buffer);
         //     exit(EXIT_SUCCESS);
         // }else{
@@ -68,7 +119,7 @@ int main(int argc, char* argv[]){
             switch (do_meta_command(input_buffer)){
                 case (META_COMMAND_SUCCESS):
                     continue;
-                case (META_COMMAND_UNRECONGNIZED_COMMAND):
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
                     printf("Unrecognized command '%s'\n", input_buffer->buffer);
                     continue;    
             }
