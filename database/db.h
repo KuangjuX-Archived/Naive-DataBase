7d15733f0b305812ffbd2d6ce6fd79b11460a98a
3019244189 QiChengXiang <18630816527@163.com>
2020-11-03 21:43:59 +0800

    fix structer

---
+++ database/db.h
@@ -0,0 +1,112 @@
+#ifndef DB_H
+#define DB_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdint.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+//define data 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+typedef struct{
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+//define the compact representation of a row
+#define SIZE_OF_ATTRIBUTE(Struct, Attribute) (sizeof(((Struct*)0)->Attribute))
+#define ID_SIZE SIZE_OF_ATTRIBUTE(Row,id)
+
+#define USERNAME_SIZE SIZE_OF_ATTRIBUTE(Row,username)
+#define EMAIL_SIZE SIZE_OF_ATTRIBUTE(Row,email)
+#define ID_OFFSET 0
+#define USERNAME_OFFSET (ID_OFFSET + ID_SIZE)
+#define EMAIL_OFFSET (USERNAME_OFFSET + USERNAME_SIZE)
+#define ROW_SIZE (ID_SIZE + USERNAME_SIZE + EMAIL_SIZE)
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+}InputBuffer;
+
+
+//define table structure
+#define PAGE_SIZE 4096
+#define TABLE_MAX_PAGES 100
+#define ROWS_PER_PAGE (PAGE_SIZE / ROW_SIZE)
+#define TABLE_MAX_ROWS (ROWS_PER_PAGE * TABLE_MAX_PAGES)
+
+typedef struct{
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+}Table;
+
+
+typedef enum{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+}MetaCommandResult;
+
+typedef enum{
+    EXECUTE_SUCCESS,
+    EXECUTE_TABLE_FULL
+}ExecuteResult;
+
+
+typedef enum{
+    PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+    StatementType type;
+    //add Row
+    Row row_to_insert;
+}Statement;
+
+InputBuffer* new_input_buffer();
+
+void read_input(InputBuffer* input_buffer);
+
+void close_input_buffer(InputBuffer* input_buffer);
+
+void print_prompt();
+
+void print_row(Row* row);
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer);
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement);
+
+Table* new_table();
+
+void free_table(Table* table);
+
+void* row_slot(Table* table, uint32_t row_num);
+
+ExecuteResult execute_insert(Statement* statement, Table* table);
+
+ExecuteResult execute_select(Statement* statement, Table* table);
+
+ExecuteResult execute_statement(Statement* statement, Table* table);
+
+void serialize_row(Row* source, void* destination);
+
+void deserialize_row(void* source, Row* destination);
+
+#endif
---
+++ database/main.c
@@ -0,0 +1,45 @@
+#include "db.h"
+
+int main(int argc, char* argv[]){
+
+    Table* table = new_table();
+    InputBuffer* input_buffer = new_input_buffer();
+    //loop
+    while(true){
+        print_prompt();
+        read_input(input_buffer);
+        printf("%ld\n",input_buffer->buffer_length);
+        if(input_buffer->buffer[0] == '.'){
+            switch (do_meta_command(input_buffer)){
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                    continue;    
+            }
+        }
+
+        Statement statement;
+        switch(prepare_statement(input_buffer,&statement)){
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_SYNTAX_ERROR):
+                printf("Syntax error. Could not parse statement.\n");
+                continue;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'.\n",input_buffer->buffer);
+                continue;
+        }
+
+        switch (execute_statement(&statement,table)){
+            case (EXECUTE_SUCCESS):
+                printf("Executed.\n");
+                break;
+
+            case (EXECUTE_TABLE_FULL):
+                printf("Error:Table full.\n");
+                break;    
+        }
+        
+    }
+}
